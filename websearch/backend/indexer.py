3da1cc80cef02592db61f1d4d66d297139dad3d6
Dmitry Ryzhikov <d.ryzhykau@gmail.com>
2016-06-23 02:16:26 +0300

    Add Indexer class.

--- websearch/backend/indexer.py
+++ websearch/backend/indexer.py
@@ -1,12 +1,61 @@
+import collections
+import math
 import re
 
 
-def words_positions(text):
-    words = re.findall(ur'\w+', unicode(text).lower(), flags=re.UNICODE)
-    word_pos = {}
-    for index, word in enumerate(words):
-        if word in word_pos:
-            word_pos[word].append(index)
+class Indexer:
+
+    def __init__(self, words_in_file):
+        self.words_in_file = words_in_file
+        self.index = {}
+        self.build_index(self.words_in_files)
+
+    @staticmethod
+    def _words_positions(text):
+        words = re.findall(ur'\w+', unicode(text).lower(), flags=re.UNICODE)
+        word_pos = {}
+        for index, word in enumerate(words):
+            if word in word_pos:
+                word_pos[word].append(index)
+            else:
+                word_pos[word] = [index]
+        return word_pos
+
+    def build_index(self, words_in_file):
+        for filename in words_in_file:
+            for word in words_in_file[filename].keys():
+                if word in self.index:
+                    self.index[word][filename] = words_in_file[filename][word]
+                else:
+                    self.index[word] = {filename: words_in_file[filename][word]}
+
+    def _one_word_query(self, word):
+        word = re.match(ur'\w+', word, flags=re.UNICODE).group()
+        if word in self.index:
+            return self.index[word].keys()
         else:
-            word_pos[word] = [index]
-    return word_pos
+            return []
+
+    def _multiple_words_query(self, words):
+        words = re.findall(ur'\b\w+\b', words, flags=re.UNICODE)
+        result = []
+        for word in words:
+            result.extend(self._one_word_query(word))
+        return result
+
+    def phrase_query(self, query):
+        words = re.findall(ur'\b\w+\b', query)
+        results, files_list = [], []
+        for word in words:
+            files_list.append(self._one_word_query(word))
+        shared_files = set(files_list[0]).intersection(*files_list)
+        for filename in shared_files:
+            words_positions = []
+            for word in words:
+                words_positions.append(self.index[word][filename])
+            for i in xrange(len(words_positions)):
+                for j in xrange(len(words_positions[i])):
+                    words_positions -= i
+            if set(words_positions[0]).intersection(*words_positions):
+                results.append(filename)
+        return results
