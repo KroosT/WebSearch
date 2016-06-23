d04db46634989df1b3c3f5888a0e5b138823e0d5
Dmitry Ryzhikov <d.ryzhykau@gmail.com>
2016-06-23 06:41:37 +0300

    Several fixes and add tf, idf computations.

--- websearch/backend/indexer.py
+++ websearch/backend/indexer.py
@@ -6,9 +6,13 @@ import re
 class Indexer:
 
     def __init__(self, words_in_file):
-        self.words_in_file = words_in_file
+        self.words_in_files = words_in_file
         self.index = {}
-        self.build_index(self.words_in_files)
+        self.build_index()
+        self.tf = {}
+        self.compute_tf()
+        self.idf = {}
+        self.compute_idf()
 
     @staticmethod
     def _words_positions(text):
@@ -21,33 +25,51 @@ class Indexer:
                 word_pos[word] = [index]
         return word_pos
 
-    def build_index(self, words_in_file):
-        for filename in words_in_file:
-            for word in words_in_file[filename].keys():
+    def build_index(self):
+        for filename in self.words_in_files.keys():
+            for word in self.words_in_files[filename].keys():
                 if word in self.index:
-                    self.index[word][filename] = words_in_file[filename][word]
+                    self.index[word][filename] = (
+                        self.words_in_files[filename][word])
                 else:
-                    self.index[word] = {filename: words_in_file[filename][word]}
+                    self.index[word] = (
+                        {filename: self.words_in_files[filename][word]})
 
-    def _one_word_query(self, word):
+    def compute_tf(self):
+        for filename in self.words_in_files.keys():
+            euclidean_norm = sum(
+                [len(self.words_in_files[filename][word]) ** 2 for word
+                 in self.words_in_files[filename].keys()]
+            ) ** 0.5
+            self.tf[filename] = {}
+            for word in self.words_in_files[filename].keys():
+                self.tf[filename][word] = (
+                    len(self.words_in_files[filename][word]) / euclidean_norm)
+
+    def compute_idf(self):
+        n = len(self.words_in_files)
+        for word in self.index:
+            self.idf[word] = math.log(n / len(self.index[word]))
+
+    def one_word_query(self, word):
         word = re.match(ur'\w+', word, flags=re.UNICODE).group()
         if word in self.index:
             return self.index[word].keys()
         else:
             return []
 
-    def _multiple_words_query(self, words):
+    def free_text_query(self, words):
         words = re.findall(ur'\b\w+\b', words, flags=re.UNICODE)
         result = []
         for word in words:
-            result.extend(self._one_word_query(word))
+            result.extend(self.one_word_query(word))
         return result
 
     def phrase_query(self, query):
         words = re.findall(ur'\b\w+\b', query)
         results, files_list = [], []
         for word in words:
-            files_list.append(self._one_word_query(word))
+            files_list.append(self.one_word_query(word))
         shared_files = set(files_list[0]).intersection(*files_list)
         for filename in shared_files:
             words_positions = []
@@ -55,7 +77,7 @@ class Indexer:
                 words_positions.append(self.index[word][filename])
             for i in xrange(len(words_positions)):
                 for j in xrange(len(words_positions[i])):
-                    words_positions -= i
+                    words_positions[i] -= i
             if set(words_positions[0]).intersection(*words_positions):
                 results.append(filename)
         return results
