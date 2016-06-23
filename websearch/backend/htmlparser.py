e2e9062bb66867187d1f766664b5b7f702eec610
Dmitry Ryzhikov <d.ryzhykau@gmail.com>
2016-06-23 09:38:13 +0300

    Add words_positions method.

--- websearch/backend/htmlparser.py
+++ websearch/backend/htmlparser.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urlparse
 from crawler import Crawler
 import indexer
+import re
 
 
 class HtmlParser(object):
@@ -40,6 +41,16 @@ class HtmlParser(object):
             except:
                 print 'Failed'
 
+    @staticmethod
+    def _words_positions(text):
+        words = re.findall(ur'\w+', unicode(text).lower(), flags=re.UNICODE)
+        word_pos = {}
+        for index, word in enumerate(words):
+            if word in word_pos:
+                word_pos[word].append(index)
+            else:
+                word_pos[word] = [index]
+        return word_pos
 
 # urls = ['http://www.tut.by']
 # h = HtmlParser(urls)
